6626aba69a8d5fb5823b7981e1ae918a15410667
Joshua Keroes <joshua@cpan.org>
2003-07-16 19:54:07 +0000

    Import of JOSHUA/Package-Alias-0.02 from CPAN.
    gitpan-cpan-distribution: Package-Alias
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JOSHUA/Package-Alias-0.02.tar.gz
    gitpan-cpan-author:       JOSHUA
    gitpan-cpan-maturity:     released

--- Alias.pm
+++ Alias.pm
@@ -2,13 +2,13 @@
 #
 # Package::Alias - Alias one namespace into another
 #
-# $Id: Alias.pm,v 1.4 2003/07/15 22:19:32 jkeroes Exp $
+# $Id: Alias.pm,v 1.5 2003/07/16 19:50:12 jkeroes Exp $
 
 package Package::Alias;
 use strict qw/vars subs/;
 use vars   qw/$VERSION $DEBUG/;
 
-$VERSION     = 0.01;
+$VERSION     = '0.02';
 $DEBUG       = 0;
 
 #------------------------------------------------------------
@@ -55,8 +55,8 @@ Package::Alias - alias one namespace into another
 =head1 SYNOPSIS
 
   use Package::Alias
-    main => 'Foo::Bar',
-    Package::Name::Simply::Too::Long::To::Use => 'Pkg';
+      main => 'Foo::Bar',
+      Package::Name::Simply::Too::Long::To::Use => 'Pkg';
 
 =head1 DESCRIPTION
 
@@ -68,8 +68,7 @@ as well.
 Chip Salzenberg says that it's not technically feasible to perform
 runtime namespace aliasing.  At compile time, Perl grabs pointers to
 functions and global vars.  Those pointers aren't updated if we alias
-the namespace at runtime. It may be possible to do this with some
-work with XS.
+the namespace at runtime. 
 
 =head1 AUTHOR
 
